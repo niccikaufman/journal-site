d76541d72bccbeeed43c19416bc622b7b9b44cb5
Nicci Kaufman <niccikaufman@gmail.com>
2021-05-12 13:36:48 -0700

    update title and body values

--- src/main.js
+++ src/main.js
@@ -8,10 +8,10 @@ import { Entry } from "./index.js";
 $(document).ready(function() {
   $('#create-entry').click(function(event){
     event.preventDefault();
-    const title = "test";
-    const body = "test";
+    const title = $('#entry-title').val();
+    const body = $('#entry-body').val();
     let entry = new Entry(title,body);
     console.log(entry);
-    $('#posted-entry').append("<p>" + entry + "</p>");
+    $('#posted-entry').append("<p>" + entry.body + "</p>");
   });
 });
