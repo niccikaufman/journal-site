68c4643c1e1ef2e0b9951d034fc4a853b1049f43
Nicci Kaufman <niccikaufman@gmail.com>
2021-05-12 12:46:41 -0700

    create webpack-troubleshoot branch

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 //back end logic
-export default class Entry {
+export class Entry {
   constructor(title,body) {
     this.title = title;
     this.body = body;
   }
-  wordCount(body) {
-
-  }
-  vowelCount(body) {
-
-  }
-  consonantCount(body){
-
-  }
-  //return the first sentence of the entry. If the sentence is over 8 words, only display those first 8 words.
-  getTeaser(body){
-
-  }
 }
+//   wordCount(body) {
+
+//   }
+//   vowelCount(body) {
+
+//   }
+//   consonantCount(body){
+
+//   }
+//   //return the first sentence of the entry. If the sentence is over 8 words, only display those first 8 words.
+//   getTeaser(body){
+
+//   }
+// }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 //UI logic
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import $ from 'jquery';
-import Entry from './index.js';
+import { Entry } from "./index.js";
+
+$(document).ready(function() {
+  $('#create-entry').click(function(event){
+    event.preventDefault();
+    const title = "test";
+    const body = "test";
+    let entry = new Entry(title,body);
+    console.log(entry);
+    $('#posted-entry').append("<p>" + entry + "</p>");
+  });
+});
